019723018013f4ebee0594cc33b5c15e6fbd688b
ViniciusDugue <103336607+ViniciusDugue@users.noreply.github.com>
2024-01-03 02:55:57 -0800

    Innitial Commit
    How to Output an image line by line, pixel by pixel. A Ray class that casts a recursive ray onto a scene with spheres. Generating pixels using antialiasing and point sampling. A simple diffuse material that resembles a matte material.

---
+++ build_and_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run CMake build from parent folder (usually its cmake --build.)
+cmake --build build
+
+# Run the executable and redirect output to image.ppm
+build/Debug/InOneWeekend.exe > ./image.ppm
+
+# run this in raytracer directory with: ./build_and_run.sh
